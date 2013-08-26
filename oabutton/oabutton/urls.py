014e82ed17743d5a5e83917accee76192e7ce6d6
Victor Ng <victor@crankycoder.com>
2013-08-26 19:12:56 -0400

    initial commit to include social_auth apis as per issue 61

--- oabutton/oabutton/urls.py
+++ oabutton/oabutton/urls.py
@@ -16,4 +16,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    # All SocialAuth URLs
+    url(r'^auth/', include('social_auth.urls')),
 )
